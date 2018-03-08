f07d71c193ea5a0b1a5c682bf994ecb8316840e3
jonitauriainen <joni.tauriainen@nedevicesw.com>
2018-03-08 09:37:57 +0200

    omnifood stuff- index should be php but now for checking it is html

---
+++ omnifood/resources/js/script.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+
+    $('.meal-photo').click(function(){
+       console.log("TESTI");
+       $('.meal-photo').addClass('hidden');
+    });
+    
+    
+    /* Sticky Navication part */
+    $('.js-section-features').waypoint(function(direction) {
+       if( direction == "down") {
+           $('nav').addClass('sticky');
+       } else {
+           $('nav').removeClass('sticky');
+       }
+    },
+    {
+       offset: '60px;'
+    });
+    
+
+    /* Scroll on buttons */
+    $('.js-scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000);
+    });        
+    
+    $('.js-scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
+    });
+    
+    
+    /* Navication scroll */
+    $(function(){
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+                || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                   if (target.length) {
+                     $('html,body').animate({
+                         scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    
+    
+    /* Animation on scroll */
+    
+    $('.js-wp-1').waypoint(function(direction) {
+        $('.js-wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js-wp-2').waypoint(function(direction) {
+        $('.js-wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+        
+    $('.js-wp-3').waypoint(function(direction) {
+        $('.js-wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js-wp-4').waypoint(function(direction) {
+        $('.js-wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    
+    /* Mobile navication */
+    $('.js-nav-icon').click(function() {
+        var nav = $('.js-main-nav');
+        var icon = $('.js-nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+    
+    /* Maps */
+    var map = new GMaps({
+      div: '.map',
+      lat: 38.7436056,
+      lng: -9.12,
+      zoom: 12
+    });
+    
+    map.addMarker({
+      lat: 38.7436056,
+      lng: -9.2304148,
+      title: 'Lisbon',
+      infoWindow: {
+          content: '<p>Our Lisbon HQ</p>'
+      }
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
